01562a10aa65d1a74f206c73164b025e5e6368eb
GitQuasar <cursedonela@yandex.ru>
2024-05-13 22:52:36 +0500

    added origins for frontend

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 import uvicorn
 
@@ -16,6 +17,16 @@ app.include_router(login_router, prefix="/api", tags=["Login Router"])
 app.include_router(admin_router, prefix="/api", tags=["Admin Router"])
 app.include_router(manager_router, prefix="/api", tags=["Manager Router"])
 app.include_router(reviewer_router, prefix="/api", tags=["Reviewer Router"])
+
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
